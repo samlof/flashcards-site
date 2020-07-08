22408afb44102d2380e91ed8cc23a046434cc4b8
Samuli Löf <samuli.lof@gmail.com>
2020-07-08 22:19:31 +0300

    Add styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
 import { AppPropsType } from "next/dist/next-server/lib/utils";
+import { ThemeProvider } from "styled-components";
 
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
 export default function App({ Component, pageProps }: AppPropsType) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,16 @@ import PostList, {
 } from "../components/PostList";
 import { initializeApollo } from "../lib/apolloClient";
 import { GetStaticProps } from "next";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
 const IndexPage = () => (
   <App>
     <Header />
+    <Title>My page</Title>
     <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
     <Submit />
     <PostList />
