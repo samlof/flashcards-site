afc67983234f1c7c005fad05847b4b54ebf41d24
Samuli Löf <samuli.lof@gmail.com>
2020-07-15 20:55:30 +0300

    Improve animation

--- components/FlipCard.tsx
+++ components/FlipCard.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactCardFlip from "react-card-flip";
 import styled from "styled-components";
+import { delayMs } from "../helpers/delay";
 
-const CardSide = styled.div`
+const CardSide = styled.div<{ front: boolean }>`
   display: flex;
   align-self: center;
   align-items: center;
   flex-direction: column;
   justify-content: center;
-  box-shadow: 0 0 10px rgba(38, 12, 12, 0.73);
+  box-shadow: ${(props) => props.front && "-"}2px 1px 10px
+    rgba(38, 12, 12, 0.73);
   height: 30vh;
   width: 20vh;
   border-radius: 10%;
@@ -23,17 +25,36 @@ interface Props {
   back: CardWord;
 }
 
-const FlipCard = ({ front, back }: Props) => {
-  const [animate, setAnim] = React.useState(false);
+const flipSpeed = 600;
+const flipSpeedS = flipSpeed / 1000;
+const flipSpeedHalf = flipSpeed / 2;
 
+const FlipCard = ({ front, back }: Props) => {
+  const [isFront, setFront] = React.useState(false);
+  const [isFrontVisible, setIsFrontVisible] = React.useState(false);
+
+  const handleFlip = async () => {
+    setFront((f) => !f);
+    await delayMs(flipSpeedHalf - 100);
+    setIsFrontVisible((f) => !f);
+  };
   return (
-    <ReactCardFlip isFlipped={animate} flipDirection="horizontal">
-      <CardSide key="front" onClick={(e) => setAnim((a) => !a)}>
+    <ReactCardFlip
+      isFlipped={isFront}
+      flipDirection="horizontal"
+      flipSpeedBackToFront={flipSpeedS}
+      flipSpeedFrontToBack={flipSpeedS}
+    >
+      <CardSide
+        key="front"
+        onClick={(e) => handleFlip()}
+        front={isFrontVisible}
+      >
         <span>{front.text}</span>
         <i>{front.lang}</i>
       </CardSide>
 
-      <CardSide key="back" onClick={(e) => setAnim((a) => !a)}>
+      <CardSide key="back" onClick={(e) => handleFlip()} front={isFrontVisible}>
         <span>{back.text}</span>
         <i>{back.lang}</i>
       </CardSide>
