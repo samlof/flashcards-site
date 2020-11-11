5b22acb335bf513d77a2014a7608da223fd1ce4f
Samuli Löf <samuli.lof@gmail.com>
2020-11-11 19:22:04 +0200

    Fix firebase breaking changes

--- frontend/src/lib/user.ts
+++ frontend/src/lib/user.ts
@@ -1,16 +1,15 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import { User } from "firebase/app";
 import { useEffect, useState } from "react";
 
 const FbAppPromise = import("./firebase").then((x) => x.FbApp);
 interface userValue {
   loading: boolean;
-  user: User | null;
+  user: firebase.default.User | null;
 }
 export const useUser: () => userValue = () => {
   const [user, setUser] = useState<userValue>({ loading: true, user: null });
   useEffect(() => {
-    let unsub: firebase.Unsubscribe;
+    let unsub: firebase.default.Unsubscribe;
     FbAppPromise.then((FbApp) => {
       unsub = FbApp.auth().onIdTokenChanged((user) => {
         setUser({ user, loading: false });
